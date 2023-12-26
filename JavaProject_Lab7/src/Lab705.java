386e50fa6c0f43b50ee142cf9e8c8792d8309450
Gpinkaew <game202548@gmail.com>
2023-12-26 14:24:36 +0700

    JavaProject_Lab7

---
+++ JavaProject_Lab7/src/Lab705.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+public class Lab705 {
+
+	public static void main(String[] args) {
+		int[] numbers = new int[5];
+		int _numbers = Integer.parseInt(JOptionPane.showInputDialog("" + "enter item number order"));
+
+	}
+	
+	
+	public static void showEven(int[] nums) {
+		
+	}
+	
+	public static void showOdd(int[] nums) {
+		
+	}
+
+}
